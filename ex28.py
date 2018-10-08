92dbda5835a9bd45cdf6a81641836526f1860531
ethunk <ethunk>
2018-10-08 13:36:11 -0400

    finished through 30

---
+++ ex28.py
@@ -0,0 +1,18 @@
+# 1. True and True
+# 2. False and True
+# 3. 1==1and2==1 4. "test" == "test"
+# 5. 1==1or2!=1
+# 6. Trueand1==1
+# 7. False and 0 != 0
+# 8. Trueor1==1
+# 9. "test" == "testing"
+# 10. 1!=0and2==1
+# 11. "test" != "testing"
+# 12. "test" == 1
+# 13. not (True and False)
+# 14. not(1==1and0!=1)
+# 15. not (10 == 1 or 1000 == 1000)
+# 16. not(1!=10or3==4)
+# 17. not ("testing" == "testing" and "Zed" == "Cool Guy") 18. 1==1and(not("testing"==1or1==0))
+# 19. "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+# 20. 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
